f24162d322c25a94e17757f8e5a7f69ca7f26288
bryanrinaldoo <bryan.rinaldo24@gmail.com>
2021-08-14 23:51:10 +0700

    about + skills done

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,24 @@ if(navClose){
 // when clicked close navbar (mobile)
 const navLink = document.querySelectorAll('.nav__link')
 navLink.forEach(n => n.addEventListener('click', closeNavbar))
+
+// skills
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toogleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i= 0; i<skillsContent.length;i++){
+        skillsContent[i].className= 'skills__content skills__close'
+    }
+
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className= 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toogleSkills)
+})
